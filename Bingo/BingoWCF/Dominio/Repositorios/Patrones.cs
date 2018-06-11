db1683c5430dabaaa69e945b0c32444b532816f5
Ricardo <rjgarbanzo@gmail.com>
2018-06-10 23:42:52 -0600

    Método Repositorio - Retorna  Booleano[,] Matriz 5x5

--- Bingo/BingoWCF/Dominio/Repositorios/Patrones.cs
+++ Bingo/BingoWCF/Dominio/Repositorios/Patrones.cs
@@ -7,15 +7,103 @@ namespace BingoWCF.Dominio.Patrones
     public class Patrones
     {
 
-        public genPatron(int [,] patron)
+        public Boolean[,] GenPatron()
         {
+            Boolean[,] resp = new Boolean[5, 5];
+            Random rnd = new Random();
+            int rndnum = rnd.Next(1,6);
 
 
+            switch (rndnum)
+            {
+                case 1:
+                   resp= cartonLleno();
+                    break;
 
+                case 2:
+                    resp= esquinas();
+                    break;
 
+                case 3:
+                    resp= letraH();
+                    break;
+
+                case 4:
+                    resp= letraX();
+                    break;
+
+                case 5:
+                    resp= letraU();
+                    break;
+
+            }
+
+            return resp;
 
 
         }
+
+
+        public Boolean[,] cartonLleno()
+        {
+            bool[,] array = { { true, true, true,true, true },
+                              { true, true, true,true, true },
+                              { true, true, true,true, true },
+                              { true, true, true,true, true },
+                              { true, true, true,true, true },};
+            return array;
+        }
+
+
+
+        public Boolean[,] esquinas()
+        {
+            bool[,] array = {   { true, false, false,false, true },
+                                { false, false, false,false, false },
+                                { false, false, false,false, false },
+                                { false,false, false,false, false },
+                                { true, false, false,false, true },};
+            return array;
+        }
+
+
+
+        public Boolean[,] letraH()
+        {
+            bool[,] array = {   { true, false, false,false, true },
+                                { true, false, false,false, true },
+                                { true, true, true, true, true },
+                                { true,false, false,false, true },
+                                { true, false, false,false, true },};
+
+            return array;
+        }
+
+
+        public Boolean[,] letraX()
+        {
+            bool[,] array = {   { true, false, false,false, true },
+                                { false, true, false,true, false },
+                                { false, false, true, false, false },
+                                { false,true, false,true, false },
+                                { true, false, false,false, true },};
+
+            return array;
+        }
+
+
+        public Boolean[,] letraU()
+        {
+            bool[,] array = {   { true, false, false,false, true },
+                                { true, false, false,false, true },
+                                { true, false, false,false, true },
+                                { true,false, false,false, true },
+                                { true, true, true,true, true },};
+            return array;
+        }
+
+
+
     }
 
 
